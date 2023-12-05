194af68f5845afffb53203f21b41fe8f879f860f
EricKao <eric40518@outlook.com>
2023-12-05 17:01:30 +0800

    ActorsService configuration with EntityBaseRepository

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -16,13 +16,18 @@ namespace eTickets.Data.Base
             _context = context;
         }
 
-        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
 
         public async Task DeleteAsync(int id)
         {
             var entity = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
             EntityEntry entityEntry = _context.Entry<T>(entity);
             entityEntry.State = EntityState.Deleted;
+            await _context.SaveChangesAsync();
         }
 
         public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
@@ -33,6 +38,7 @@ namespace eTickets.Data.Base
         {
             EntityEntry entityEntry = _context.Entry<T>(entity);
             entityEntry.State = EntityState.Modified;
+            await _context.SaveChangesAsync();
         }
     }
 }
