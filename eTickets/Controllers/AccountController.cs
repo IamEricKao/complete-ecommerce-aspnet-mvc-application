b5f03a680d1540c8ee98b29ba4ba6e2afc1efbb2
EricKao <eric40518@outlook.com>
2024-02-20 12:35:54 +0800

    Designing Login view

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets.Data;
+using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -23,9 +24,10 @@ namespace eTickets.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Login()
         {
-            return View();
+            var response = new LoginVM();
+            return View(response);
         }
     }
 }
---
+++ eTickets/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "這不是有效的Email")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "電子郵件")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "請輸入密碼")]
+        [DataType(DataType.Password)]
+        [Display(Name = "密碼")]
+        public string Password { get; set; }
+    }
+}
